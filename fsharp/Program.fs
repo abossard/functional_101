3874f2e633812933725cf1af0b3e9125a0bb7915
Andre Bossard <anbossar@microsoft.com>
2022-04-21 20:44:49 +0200

    init

---
+++ fsharp/Program.fs
@@ -0,0 +1,31 @@
+﻿type Request = string
+type Command = { Name: string; Body: Request }
+type SQL = string
+
+let requestToCommand request =
+    // split string
+    let name = request.ToString().Split("/")[1]
+    { Name = name; Body = request }
+
+let commandToSqlWith operation command = $"{operation} * from %s{command.Name}"
+
+let validateCommand command =
+    match command.Name with
+    | null -> Error "No name found."
+    | "" -> Error "Name is empty."
+    | "bananas" -> Error "Bananas is not a name."
+    | _ -> Ok command
+
+let result =
+    "GET /bananas/index.html"
+    |> requestToCommand
+    |> Ok
+    |> Result.bind validateCommand
+    |> function
+        | Ok command -> command
+        | _ ->
+            { Name = "Unsupported"
+              Body = "Unsupported command." }
+    |> commandToSqlWith "select"
+
+printfn $"Command is: %A{result}"
